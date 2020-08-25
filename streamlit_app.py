091655397b4a26ef49370f996f09cb4b8ea1ed55
Ángel Ruiz-Peinado <57810653+angelrps@users.noreply.github.com>
2020-08-25 17:45:17 +0200

    debugging_all removed apart from st.title("TEST")

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,18 +9,5 @@ import altair as alt
 
 #############################   DEFINE FUNCTIONS START   #############################
 
-def get_LocationIDs():
-    # 1. Import Location and Borough columns form NY TAXI ZONES dataset
-    dfzones = pd.read_csv('NY_taxi_zones.csv', sep=',',
-                          usecols=['LocationID', 'borough'])
 
-    # 2. Filter Manhattan zones
-    dfzones = dfzones[dfzones['borough']=='Manhattan']\
-                    .drop(['borough'], axis=1)\
-                    .sort_values(by='LocationID')\
-                    .drop_duplicates('LocationID').reset_index(drop=True)    
-    return dfzones
-
-# RUN FUNCTIONS
-df_zones = get_LocationIDs()
-st.write(df_zones)
+st.title("TEST")
